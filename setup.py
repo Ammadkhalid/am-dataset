f01136b2f9c1123264978c58b9928a7664ebb94a
AmmadKhalid <ammadkhalid12@gmail.com>
2018-04-29 04:56:32 +0500

    Package Published.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ name = 'amdataset',
 version = '0.1',
 packages = find_packages(),
 author = 'Ammad Khalid',
+description = open('README.md', 'r').read(),
 install_requires = ['dataset'],
+url = '',
 author_email = 'Ammadkhalid12@gmail.com'
 )
