4097131fdb432d621ffa5bfe116d1c282a3018ca
ammadkhalid <ammadkhalid12@gmail.com>
2018-11-11 16:56:35 +0500

    version problem.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
 name = 'amdataset',
-version = '0.2.1',
+version = '0.2.2',
 packages = find_packages(),
 author = 'Ammad Khalid',
 description = open('README.md', 'r').read(),
